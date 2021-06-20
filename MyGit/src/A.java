f7381dc8e270fac21072d57e9767dace2a3e2927
홍유진 <@DESKTOP-0MT66Q0>
2021-06-21 02:32:53 +0900

    Commit and Push

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -1,9 +1,11 @@
 
 public class A {
 
-	public static void sayHello() {
-		System.out.println("Hello Git!");
+	 public static void main(String args[]) {
+			
+		 B B = new B();
+		 B.sayHello();
 
-	}
+	 }
 
 }
